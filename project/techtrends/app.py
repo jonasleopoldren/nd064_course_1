247a82ce6216daf5b1525043a99b96ed666d2f9d
Jonas Leopold <jonas.leopold@renesas.com>
2024-06-20 11:10:35 +0200

    techtrends project step 3

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,4 +1,8 @@
 import sqlite3
+import logging
+import sys
+
+from logging.config import dictConfig
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
@@ -8,6 +12,8 @@ from werkzeug.exceptions import abort
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection.execute('INSERT INTO connections DEFAULT VALUES')
+    connection.commit()
     return connection
 
 # Function to get a post using its ID
@@ -36,13 +42,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.info(f'Non existing article requested. ID: {post_id}')
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.info(f'Article "{post[2]}" retrieved!')
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About page retrieved!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +69,79 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info(f'New article created: "{title}"!')
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+
+# Healthcheck endpoint
+# Build the /healthz endpoint for the TechTrends application. The endpoint should return the following response:
+# - An HTTP 200 status code
+# - A JSON response containing the 'result: OK - healthy' message
+@app.route('/healthz')
+def healthz():  
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
+
+# Metrics endpoint
+# Build a /metrics endpoint that would return the following:
+# - An HTTP 200 status code
+# - A JSON response with the following metrics:
+#   -  Total amount of posts in the database
+#   -  Total amount of connections to the database. For example, accessing an article will query the database, hence will count as a connection.
+# Example output: {"db_connection_count": 1, "post_count": 7}
+# Tips: The /metrics endpoint response should NOT be hardcoded.
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connections = connection.execute('SELECT * FROM connections').fetchall()
+    connection.close()
+    db_connection_count = len(connections)
+    post_count = len(posts)
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": post_count}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
+
+# Logs
+# Extend the TechTrends application to log the following events:
+# - An existing article is retrieved. The title of the article should be recorded in the log line.
+# - A non-existing article is accessed and a 404 page is returned.
+# - The "About Us" page is retrieved.
+# - A new article is created. The title of the new article should be recorded in the logline.
+# Every log line should include the timestamp and be outputted to the STDOUT. Also, capture any Python logs at the DEBUG level.
+# Example output of loglines:
+# INFO:werkzeug:127.0.0.1 - - [08/Jan/2021 22:40:06] "GET /metrics HTTP/1.1" 200 -
+# INFO:werkzeug:127.0.0.1 - - [08/Jan/2021 22:40:09] "GET / HTTP/1.1" 200 -
+# INFO:app:01/08/2021, 22:40:10, Article "2020 CNCF Annual Report" retrieved!
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    # https://flask.palletsprojects.com/en/2.3.x/logging/#basic-configuration
+    dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+    })
+    app.run(host='0.0.0.0', port=3111)
